27e7160edf35e36488373e66ce563fc9d92813fa
wangfeng7399 <wangfeng17399@163.com>
2018-08-16 15:50:49 +0800

    删除冗余代码

--- src/components/common/LeftSider/dockersider.jsx
+++ src/components/common/LeftSider/dockersider.jsx
@@ -1,7 +1,6 @@
 import React ,{Component}from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router'
-import { connect } from 'react-redux';
 import { Menu } from 'antd';
 import './index.less';
 
