6d2458b331243cc46b4d568c634897b4c1c8f5be
wangfeng7399 <wangfeng17399@163.com>
2018-09-03 14:16:35 +0800

    新增国际化

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
+import { LocaleProvider } from 'antd';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+import 'moment/locale/zh-cn';
 
 import store from './redux/index';
 // console.log(store.getState())
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><LocaleProvider locale={zh_CN}><App /></LocaleProvider></Provider>, document.getElementById('root'));
