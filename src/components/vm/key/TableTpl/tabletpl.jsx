37d56e16b9b489b0db9d573b7def4f08c426f2e1
tanweizuo <admin@0x55aa.com>
2018-09-05 11:48:19 +0800

    密钥详情

--- src/components/vm/key/TableTpl/tabletpl.jsx
+++ src/components/vm/key/TableTpl/tabletpl.jsx
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 
+import KeyDetail from '../KeyDetail'
+
 export function getColumes() {
-    return [{
-        title: '名称',
-        dataIndex: 'name',
-    }, {
-        title: '指纹',
-        dataIndex: 'fingerprint'
-    }, {
-        title: '创建时间',
-        dataIndex: 'created_at',
-    },{
-        title: '操作',
-        render: (data) => (
-            <div>
-                <span style={{cursor: 'pointer',color:'#0350CF',marginRight:30}}>详情</span>
-                <span style={{cursor: 'pointer',color:'#0350CF',marginRight:30}}>删除</span>
-            </div>
-        )
-    }];
+  return [{
+    title: '名称',
+    dataIndex: 'name',
+  }, {
+    title: '指纹',
+    dataIndex: 'fingerprint'
+  }, {
+    title: '创建时间',
+    dataIndex: 'created_at',
+  },{
+    title: '操作',
+    render: (record) => (
+      <div>
+        <KeyDetail record={ record } />
+        <span className="ant-divider" />
+        <span style={{cursor: 'pointer',color:'#0350CF',marginRight:30}}>删除</span>
+      </div>
+    )
+  }];
 }
