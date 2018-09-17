97573a3602472a71610299f31c368eba7596ac36
qinhairong <878172421@qq.com>
2018-09-17 16:57:18 +0800

    组列表移动到slowquery中

---
+++ src/components/daas/slowquery/group.jsx
@@ -0,0 +1,252 @@
+import React, { Component } from 'react';
+import qs from 'qs'
+import { message, Layout, Form, Input, Button, Select, Table } from 'antd';
+import { connect } from 'react-redux';
+import axios from 'axios';
+
+import Daassider from '../../../components/common/LeftSider/daassider';
+import { getGroupsList } from '../../../containers/Daas/groups.redux'
+
+import { GroupCreateForm } from './groupforms/groupcreateform'
+import {GroupUpdateForm} from './groupforms/groupupdateform'
+
+import { postAjax } from '../../../utils/axios'
+import BreadcrumbCustom from '../../BreadcrumbCustom';
+import { getgroups } from './TableTpl/group';
+import './list.less';
+
+const baseUrl = 'http://127.0.0.1:8000';
+var instance = axios.create({
+    baseURL: baseUrl,//测试环境
+    timeout: 2000,
+});
+instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
+
+const { Sider, Content } = Layout;
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+class DaasGroupManageForm extends Component {
+    constructor(props) {
+        super(props);
+        this.columns = getgroups.call(this);
+    }
+    state = {
+        currentPage: 1,
+        pageSize: 10,
+        updateRecord:{fields:{}},
+        GroupCreateVisible: false,
+        GroupCreateConfirmLoading: false,
+        GroupUpdateVisible: false,
+        GroupUpdateConfirmLoading: false,
+    }
+    componentDidMount () {
+        this.props.getGroupsList({});
+    }
+    //重置表单
+    handleReset = () => {
+        this.props.form.resetFields();
+    }
+
+    showGroupCreateModel = () => {
+        this.setState({GroupCreateVisible: true}) 
+    }
+    
+    showGroupUpdateModel = (record) => {
+        this.setState({GroupUpdateVisible: true}) 
+        this.setState({updateRecord: record})
+
+    }
+
+    handleGroupCreateCancel = () => {
+        this.setState({GroupCreateVisible: false}) 
+        this.setState({GroupCreateConfirmLoading: false})
+        const form = this.GroupCreateFormRef.props.form;
+        form.resetFields();
+    }
+
+    handleGroupUpdateCancel = () => {
+        this.setState({GroupUpdateVisible: false}) 
+        this.setState({GroupUpdateConfirmLoading: false})
+        const form = this.GroupUpdateFormRef.props.form;
+        form.resetFields();
+    }
+
+    handleGroupListWithArgs = (page, pageSize) => {
+       let value = this.props.form.getFieldsValue()
+       this.setState({
+           currentPage: page,
+           pageSize: pageSize 
+       })
+       let page_args = {page: page, pagesize: pageSize}
+       page_args.name = value.name !== undefined ? value.name : "";
+       this.props.getGroupsList(page_args);
+    }
+
+    handleGroupCreate = () => {
+      const form = this.GroupCreateFormRef.props.form;
+      form.validateFields((err, values) => {
+        if (err) {
+          return;
+        }
+        this.setState({GroupCreateConfirmLoading: true})
+        const _that = this;
+        console.log(values);
+        instance.post('/v1/api/slow/query/groups', qs.stringify(values))
+          .then(function(res){
+            if(res.data.code == 0){
+                message.success("创建成功") 
+                _that.setState({GroupCreateConfirmLoading: false})
+                form.resetFields();
+                _that.setState({ GroupCreateVisible: false });
+                _that.handleGroupListWithArgs(1, 10);
+            }else{
+                message.error(res.data.message) 
+                _that.setState({GroupCreateConfirmLoading: false})
+            }
+        })
+      });
+    }
+
+    handleGroupUpdate = (group_id) => {
+      const form = this.GroupUpdateFormRef.props.form;
+      form.validateFields((err, values) => {
+        if (err) {
+          return;
+        }
+        this.setState({GroupUpdateConfirmLoading: true})
+        const _that = this;
+        console.log(values);
+        // values.pk = group_id
+        instance.put('/v1/api/slow/query/groups/' +  group_id.pk, qs.stringify(values))
+          .then(function(res){
+            if(res.data.code == 0){
+                console.log(values)
+                message.success("更新成功") 
+                _that.setState({GroupUpdateConfirmLoading: false})
+                form.resetFields();
+                _that.setState({ GroupUpdateVisible: false });
+                _that.setState({ GroupCreateVisible: false });
+                _that.handleGroupListWithArgs(1, 10);
+            }else{
+                message.error(res.data.message) 
+                _that.setState({GroupUpdateConfirmLoading: false})
+            }
+        })
+      });
+    }
+
+
+    saveGroupCreateFormRef = (formRef) => {
+      this.GroupCreateFormRef = formRef;
+    }
+
+    saveGroupUpdateFormRef = (formRef) => {
+      this.GroupUpdateFormRef = formRef;
+    } 
+
+    handleGroupsQuery = () => {
+        let value = this.props.form.getFieldsValue()
+        this.setState({
+            currentPage: 1,
+            pageSize: 10 
+        })
+        let page_args = {page: 1, pagesize: 10}
+        page_args.name = value.name !== undefined  ? value.name : "";
+        this.props.getGroupsList(page_args);
+    }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    let _that = this;
+    const pagination = {
+          current: this.state.currentPage,
+          total: this.props.total,
+          pageSize: this.state.pageSize,
+          showSizeChanger: true,
+          showTotal:(total, range) => `${range[0]}-${range[1]} of ${total} items`,
+          showQuickJumper: true,
+          //当表格有变化时，如：点击分页  current是当前页面页码
+          onChange(page, pageSize) {
+              let value = _that.props.form.getFieldsValue()
+              _that.setState({
+                  currentPage: page,
+                  pageSize: pageSize
+              })
+              let page_args = {page, page_size: pageSize}
+              page_args.name = value.name !== undefined  ? value.name : "";
+              _that.props.getGroupsList({page_args});
+          },
+
+          onShowSizeChange(current, size) {
+              let value = _that.props.form.getFieldsValue()
+              _that.setState({
+                  currentPage: 1,
+                  pageSize: size 
+              })
+              let page_args = {page: 1, page_size: size}
+              page_args.name = value.name !== undefined  ? value.name : "";
+              _that.props.getGroupsList({page_args});
+          }
+    };
+
+    return (
+      <Layout className="config">
+        <Sider >
+            <Daassider/>
+        </Sider>
+        <Content style={{ padding: 0, margin:10, marginBottom: 0, minHeight: window.innerHeight-84 }}>
+            <BreadcrumbCustom first="关系型数据库" second="慢日志查询组列表" />
+            <div className="form-search-box" style={{ background:'#fff',padding:10, }}>
+                <Form layout="inline" onSubmit={this.handleSubmit}>
+                    <FormItem>
+                        <Button type="primary" onClick={this.showGroupCreateModel}>创建邮件组</Button>
+                        {/*<Button type="primary" onClick={(e) => this.openAddDevicePage('add',e)}>创建邮件组</Button>*/}
+                		<GroupCreateForm
+        				  wrappedComponentRef={this.saveGroupCreateFormRef}
+        				  visible={this.state.GroupCreateVisible}
+                          confirmLoading={this.state.GroupCreateConfirmLoading}
+        				  onCancel={this.handleGroupCreateCancel}
+        				  onCreate={this.handleGroupCreate}
+        				/>
+                        <GroupUpdateForm
+                          wrappedComponentRef={this.saveGroupUpdateFormRef}
+                          visible={this.state.GroupUpdateVisible}
+                          groupinfo={this.state.updateRecord}
+                          confirmLoading={this.state.GroupUpdateConfirmLoading}
+                          onCancel={this.handleGroupUpdateCancel}
+                          onCreate={() => this.handleGroupUpdate(this.state.updateRecord)}
+                        />       
+                 
+                    </FormItem>
+                    <div style={{ float:'right'}}>
+
+                        <FormItem label="">
+                            {getFieldDecorator('name')(
+                                <Input placeholder="组名" />
+                            )}
+                        </FormItem>
+                    <FormItem>
+                        <Button type="primary" className="btn-search" style={{marginRight: 10}} onClick={this.handleGroupsQuery}>查询</Button>
+                        <Button className="btn-search" onClick={this.handleReset}>重置</Button>
+                    </FormItem>
+                    </div>
+                </Form>
+            </div>
+
+            <div style={{ background:'#fff' }}>
+                <Table bordered loading={this.props.loading} rowKey={record => record.pk} columns={this.columns} dataSource={this.props.groupsList} pagination={pagination} />
+            </div>
+            <div style={{margin: 20}}>
+                <span className='num'>共找到 { this.state.total }条结果， 每页显示10条</span>
+            </div>
+        </Content>
+      </Layout>
+    );
+  }
+}
+
+const DaasGroupManage = Form.create()(DaasGroupManageForm);
+export default connect(
+  state => state.daasGroups,
+  { getGroupsList })(DaasGroupManage);
--- src/router/daas.js
+++ src/router/daas.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 
-import SlowQuery_Group from '../components/daas/slowquery/grouplist';
-import SlowQuery_User from '../components/daas/slowquery/userlist';
 import Daas_Instance from '../components/daas/sql/Instance';
 import Daas_Cluster from '../components/daas/sql/cluster';
 import Daas_Backup from '../components/daas/sql/backup';
 import Daas_Binlog from '../components/daas/sql/binlog';
 import Daas_Verify from '../components/daas/sql/verify';
-import Daas_Group from '../components/daas/sql/group';
+import Daas_Group from '../components/daas/slowquery/group';
+// import Daas_Group from '../components/daas/sql/group';
 import Daas_User from '../components/daas/sql/user';
 import Daas_Email from '../components/daas/sql/email';
 import Daas_Parameter from '../components/daas/sql/parameter';
@@ -55,13 +54,5 @@ const daas_routes=
         exact: true,
         path: '/daas/machine',
         component: Daas_Machine,
-    },{
-        exact: true,
-        path: '/daas/slowquery/grouplist',
-        component: SlowQuery_Group,
-    },{
-        exact: true,
-        path: '/daas/slowquery/userlist',
-        component: SlowQuery_User,
     }]
 export default daas_routes;
